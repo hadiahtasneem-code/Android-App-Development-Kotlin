e9081d4574b219b15034747cf0cfc57394fddb7d
hadiah-Tasneem <hadiyanit095@gmail.com>
2026-04-08 00:30:37 +0530

    Add copyright and license information to MainActivity.kt
    Added copyright notice for 2023 and license information.

--- Desert App/MainActivity.kt
+++ Desert App/MainActivity.kt
@@ -1,4 +1,4 @@
-
+/*
  * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
  
 
 package com.example.dessertclicker
